9a23ca27743a5f8ce43021611a93b09384f00bc0
govindchari <govindchari1@gmail.com>
2022-07-18 20:18:30 -0700

    Removed .vscode

--- .vscode/extensions/julialang.language-julia-1.6.24/scripts/packages/LanguageServer/test/test_misc.jl
+++
@@ -1,5 +0,0 @@
-@testset "get_file_loc" begin
-    str = "(x+y for x in X for y in Y if begin if true end end)"
-    cst = CSTParser.parse(str)
-    @test LanguageServer.get_file_loc(cst[2][5][1][1]) == (nothing, 20)
-end
